59358630809b96c8d2e48f0d51471577a932f3c5
Madhu <madhu9sudhana@gmail.com>
2017-06-24 23:06:02 +0530

    QQ Plot

--- Stats2.R
+++ Stats2.R
@@ -1,3 +1,15 @@
 distr  <- c("logistic","normal","exponential","poisson")
 
 fitData(a, fit = distr, sample = 1)
+
+X <- c(-4, -3, -2, -1, 0, 1, 2, 3, 4)
+Y <- c(10, 36, 91, 223, 444, 254, 108, 43, 10)
+
+# random normal variables fit the line quite well
+qqnorm(rnorm(50));abline(0,1)
+
+# random uniformly distributed variables do not
+qqnorm(rnorm(50));abline(0,1)
+
+# Q-Q plot with your data
+qqnorm(Y);abline(0,1)
